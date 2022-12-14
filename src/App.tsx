7015bee835b24c758a51d90f4f20611a0cca7ef6
okada takuma <takuma@pop-os.localdomain>
2022-12-14 15:16:03 +0900

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { motion } from 'framer-motion'
-import { useCallback, useState } from 'react'
 import { NotificationProvider } from './components/Notification'
 import { Sidebar } from './components/Sidebar'
 import { RequestBook } from './Feature/RequestBook'
 
 const App = () => {
-  const [showSidebar, setShow] = useState(false)
-  const handleOnShowSidebar = useCallback(() => setShow(true), [])
-  const handleOnCloseSidebar = useCallback(() => setShow(false), [])
   return (
     <div className='min-w-[600px]'>
       <NotificationProvider>
-        <motion.button
-          whileHover={{ scale: 1.3 }}
-          onClick={handleOnShowSidebar}
-          className='text-4xl font-bold mt-5 mx-10'
-        >
-          B
-        </motion.button>
-        <Sidebar show={showSidebar} onClose={handleOnCloseSidebar} />
-        <RequestBook />
+        <Sidebar>
+          <RequestBook />
+        </Sidebar>
       </NotificationProvider>
     </div>
   )
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,4 +1,5 @@
 import { AnimatePresence, motion } from 'framer-motion'
+import { ReactNode, useCallback, useState } from 'react'
 
 const variants = {
   hidden: {
@@ -27,35 +28,49 @@ const variants = {
   },
 }
 type SidebarProps = {
-  show: boolean
-  onClose: () => void
+  children: ReactNode
 }
-export const Sidebar = ({ show, onClose }: SidebarProps) => {
+export const Sidebar = ({ children }: SidebarProps) => {
+  const [show, setShow] = useState(false)
+  const handleOnShowSidebar = useCallback(() => setShow(true), [])
+  const handleOnCloseSidebar = useCallback(() => setShow(false), [])
+
   return (
-    <AnimatePresence>
-      {show && (
-        <motion.div
-          initial='hidden'
-          animate='visible'
-          exit='hidden'
-          variants={variants}
-          className='w-full h-screen bg-gray-400 text-white px-10 fixed top-0 left-0 pt-5'
-        >
-          <motion.button
-            className='text-4xl font-bold'
-            onClick={onClose}
-            whileHover={{ scale: 1.3 }}
+    <>
+      <motion.button
+        whileHover={{ scale: 1.3 }}
+        onClick={handleOnShowSidebar}
+        className='text-4xl font-bold mt-3 mx-10'
+      >
+        B
+      </motion.button>
+      {children}
+      <AnimatePresence>
+        {show && (
+          <motion.div
+            initial='hidden'
+            animate='visible'
+            exit='hidden'
+            variants={variants}
+            className='w-full h-screen bg-gray-400 text-white px-10 fixed top-0 left-0 pt-3'
           >
-            B
-          </motion.button>
-          <ul className='mt-5 ml-5 space-y-3'>
-            <LinkItem key='link-1' text='本を探す.' />
-            <LinkItem key='link-2' text='申請を確認する.' />
-            <LinkItem key='link-3' text='本を登録する.' />
-          </ul>
-        </motion.div>
-      )}
-    </AnimatePresence>
+            <motion.button
+              className='text-4xl font-bold'
+              onClick={handleOnCloseSidebar}
+              whileHover={{ scale: 1.3 }}
+            >
+              B
+            </motion.button>
+            <ul className='mt-5 ml-5 space-y-3'>
+              <LinkItem key='link-1' text='本を探す.' />
+              <LinkItem key='link-2' text='申請を確認する.' />
+              <LinkItem key='link-3' text='本を登録する.' />
+            </ul>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
+
   )
 }
 
