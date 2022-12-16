a511ba1e6e9420006b154bab204bacdf1de50fd7
okada takuma <takuma@pop-os.localdomain>
2022-12-16 10:54:36 +0900

    use isbn13

--- src/features/AddBook/api.ts
+++ src/features/AddBook/api.ts
@@ -27,13 +27,26 @@ export const findByKeyword = (keyword: string) =>
     params: { q: keyword },
   }).then((res) =>
     res.data.items.map(
-      ({ volumeInfo }) =>
-        ({
-          isbn: volumeInfo.industryIdentifiers[0].identifier,
+      ({ volumeInfo }) => {
+        return {
+          isbn: getIsbn(volumeInfo.industryIdentifiers, volumeInfo.title),
           title: volumeInfo.title,
           imgSrc: volumeInfo.imageLinks?.thumbnail,
-        } as BookInfo)
+        } as BookInfo
+      }
     )
   )
+
+const getIsbn = (identifiers: IndustryIdentifiersType[], bookTitle: string): string => {
+  const isbn10 = identifiers.find(({ type }) => type === 'ISBN_10')
+  const isbn13 = identifiers.find(({ type }) => type === 'ISBN_13')
+  if (isbn13) {
+    return isbn13.identifier
+  }
+  if (isbn10) {
+    return isbn10.identifier
+  }
+  throw new Error(`Not exists isbn [title:${bookTitle}]`)
+}
 
 export const addBook = (isbn: string) => BookApi.post('/books', { isbn })
